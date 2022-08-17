ca91d0b9ca8462969339ddc829bc221c960b5e62
Pranitapd <49433885+Pranitapd@users.noreply.github.com>
2022-08-17 21:03:14 +0530

    spring boot initializer files

---
+++ src/main/java/com/ancient/verdicto/VerdictoApplication.java
@@ -0,0 +1,13 @@
+package com.ancient.verdicto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdictoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerdictoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ancient/verdicto/VerdictoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ancient.verdicto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerdictoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
